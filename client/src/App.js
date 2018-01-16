7d49afad26cca4d354beb089696ca705e48352e1
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2018-01-16 17:22:29 +0100

    Test connection to API server

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  state = {tasks: []}
+  
+    componentDidMount() {
+      fetch('/tasks')
+        .then(res => res.json())
+        .then(tasks => this.setState({ tasks }));
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <h1>Tasks</h1>
+          {this.state.tasks.map(user =>
+            <div key={user.id}>{user.name}</div>
+          )}
+        </div>
+      );
+    }
   }
-}
 
 export default App;
