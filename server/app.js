41f491467116316feccbc755d21ac4a28d146069
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2018-03-06 13:45:05 +0100

    edit config for running both server/client with one command

--- server/app.js
+++ server/app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -24,7 +20,7 @@ app.use(bodyParser.json());
 
 
 // Route for handling File updates.
-let fileUpload = require('./api/routes/filesRoutes');
+var fileUpload = require('./api/routes/filesRoutes');
 fileUpload(app);
 
 
@@ -41,11 +37,9 @@ partner_routes(app);
 app.use(logger('dev'));
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use(express.static(path.join(__dirname, 'public')));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
