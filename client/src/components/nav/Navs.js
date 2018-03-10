2f903f366efba52f97726a1e588967c37df527d3
Sajeevan Puvikaran <Sajeevan96@hotmail.fr>
2018-03-10 18:52:26 +0100

    main page

---
+++ client/src/components/nav/Navs.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Nav, NavItem, NavLink} from 'reactstrap';
+
+
+console.log("Passed");
+class Navs extends React.Component {
+	render() {
+		return (
+			
+				<Nav>
+					<NavItem>
+						<img src="./logo_pulv.png" />
+					</NavItem>
+					<NavItem>
+						<NavLink href="#">Home</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="#">Projets</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="#">Déposer un projet</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="#">Se connecter</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="#">Admin</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="#">Fr</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="#">En</NavLink>
+					</NavItem>
+					<hr />
+				</Nav>
+		);
+	}
+}
+export default Navs;
