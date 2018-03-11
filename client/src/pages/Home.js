e67e949a399c828fff5da3c9e5d6f6ac3df58b19
Sajeevan Puvikaran <Sajeevan96@hotmail.fr>
2018-03-11 14:11:57 +0100

    commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -24,7 +24,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Navs /><hr/>
+        <Navs />
         <JumbotronPresentation />
         <JumbotronPULV />
         <Carousel />
