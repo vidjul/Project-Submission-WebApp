b3cfda10e07c5ceec6f2064e7d847bdb283e46e0
ThomasDenneulin <thomas.denneulin@devinci.fr>
2018-03-21 18:12:25 +0100

    flag in navbar

--- client2/src/components/nav/Navs.js
+++ client2/src/components/nav/Navs.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Nav, NavItem, NavLink, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
 import ToolbarGroup from 'material-ui/Toolbar/ToolbarGroup'
-import {Link} from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import {
+	Icon_Flag_US,
+	Icon_Flag_FR
+  } from 'material-ui-country-flags';
 class Navs extends React.Component {
 	constructor(props) {
 		super(props)
@@ -12,8 +16,8 @@ class Navs extends React.Component {
 			title: {
 				cursor: 'pointer',
 			},
-			menuItem : {
-				color : 'white',
+			menuItem: {
+				color: 'white',
 			},
 		};
 	}
@@ -23,34 +27,52 @@ class Navs extends React.Component {
 	}
 
 	handleButtonClick(event) {
-		
+
 	}
 
 	render() {
 
 		var configItem = [
-			{label : "Home" , href : "/"},
-			{label : "Projets" , href : "/Projects"},
-			{label : "Déposer un projet" , href : "/Deposit"},
-			{label : "Se connecter" , href : "/Connection"},
-			{label : "Admin" , href : "/Admin"},
-			{label : "Lien projet perdu", href : "/forgot"},
-			{label : "FR" , href : "#"},
-			{label : "EN" , href : "#"}
+			{ label: "Home", href: "/" },
+			{ label: "Projets", href: "/Projects" },
+			{ label: "Déposer un projet", href: "/Deposit" },
+			{ label: "Se connecter", href: "/Connection" },
+			{ label: "Admin", href: "/Admin" },
+			{ label: "Lien projet perdu", href: "/forgot" },
+			{ label: "FR", href: "#", icon: true },
+			{ label: "EN", href: "#", icon: true }
 		]
-		var menu = <div> {configItem.map( (item) =>
-	<Link key = {item.label} to = {item.href}>
-		<FlatButton label = {item.label} style = {this.styles.menuItem} />
-		</Link>
-		)}
+		var menu = <div> {configItem.map((item) => {
+			if (item.icon) {
+				switch(item.label){
+					case "FR" :
+					return <Link key={item.label} to={item.href}>
+					<IconButton>
+					<Icon_Flag_FR/>
+					</IconButton>
+				</Link>
+				case "EN" :
+				return <Link key={item.label} to={item.href}>
+				<IconButton>
+				<Icon_Flag_US/>
+				</IconButton>
+			</Link>
+				}
+				
+			} else {
+				return <Link key={item.label} to={item.href}>
+					<FlatButton label={item.label} style={this.styles.menuItem} />
+				</Link>
+			}
+		})}
 		</div>
 		return (
 			<div>
 				<AppBar
-					title={<img style = {this.styles.title} src="/logo_pulv.png" height="50" width="50" />}
+					title={<img style={this.styles.title} src="/logo_pulv.png" height="50" width="50" />}
 					onTitleClick={this.handleclick.bind(this)}
 					iconElementRight={menu}
-					showMenuIconButton = {false} />
+					showMenuIconButton={false} />
 			</div>
 		);
 	}
