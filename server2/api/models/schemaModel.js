81cb1a3c6d5878c9ebeb6983a983af1891819f81
Sajeevan Puvikaran <Sajeevan96@hotmail.fr>
2018-03-21 23:07:11 +0100

    change schema

--- server2/api/models/schemaModel.js
+++ server2/api/models/schemaModel.js
@@ -73,7 +73,6 @@ module.exports = mongoose.model('Project', ProjectSchema);
 //Specialization Class
 var SpecializationSchema = new Schema({
 	school_name: {type: String, required: true},
-	study_year: {type: String, required: true}, 
 	major_name: {type: String, required: true},
 	referent: {type: String, required: true}
 });
