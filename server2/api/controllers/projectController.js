f5cb6986bb89603707f7b82cd9abd06e6d307df4
Sajeevan Puvikaran <Sajeevan96@hotmail.fr>
2018-03-26 14:33:33 +0200

    fix admin

--- server2/api/controllers/projectController.js
+++ server2/api/controllers/projectController.js
@@ -4,7 +4,7 @@ const generator = require('generate-password');
 var mongoose = require('mongoose');
 const Project = mongoose.model('Project');
 const Partner = mongoose.model('Partner');
-const Admin = mongoose.model('Admin');
+const Admin = mongoose.model('Administration');
 
 const mailer = require('nodemailer');
 const smtpTransporter = mailer.createTransport({
