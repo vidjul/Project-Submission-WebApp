c0e48aaba66eb79e9bbedc61490dc9c903a54758
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2018-03-27 17:30:08 +0200

    translation fix

--- client2/src/App.js
+++ client2/src/App.js
@@ -15,7 +15,7 @@ import Nav from './components/nav/Navs'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { lng: 'en' }
+    this.state = { lng: 'fr' }
     this.handleLngChange = this.handleLngChange.bind(this);
   }
 
