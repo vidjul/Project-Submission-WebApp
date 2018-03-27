a0c9c42065d53db60d54a6305d42dc8018b6bf8e
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2018-03-27 17:53:14 +0200

    edit fix

--- client2/src/App.js
+++ client2/src/App.js
@@ -30,16 +30,16 @@ class App extends Component {
         <div>
           <Nav lng={lng} handleLngChange={this.handleLngChange} />
           <Switch>
-            <Route exact path="/" render={() => (<Home lng={lng} />)} />
-            <Route exact path="/Projects" render={() => (<Projects lng={lng} />)} />
-            <Route exact path="/Deposit" render={() => (<Deposit lng={lng} />)} />
-            <Route exact path="/Connection" render={() => (<Connection lng={lng} />)} />
-            <Route exact path="/Admin" render={() => (<Admin lng={lng} />)} />
-            <Route exact path="/Edit/:editKey" render={() => (<EditDeposit lng={lng} />)}/>
-            <Route exact path="/Forgot" render={() => (<ForgetPass lng={lng} />)}/>
-            <Route exact path="/Admin/Validation" render={() => (<ProjectValidation lng={lng} />)}/>
-            <Route exact path="/login" render={() => (<Login lng={lng} />)}/>
-            <Route exact path="/protected" render={() => (<Protected lng={lng} />)}/>
+            <Route exact path="/" render={(routeProps) => (<Home lng={lng} {...routeProps} />)} />
+            <Route exact path="/Projects" render={(routeProps) => (<Projects lng={lng} {...routeProps} />)} />
+            <Route exact path="/Deposit" render={(routeProps) => (<Deposit lng={lng} {...routeProps} />)} />
+            <Route exact path="/Connection" render={(routeProps) => (<Connection lng={lng} {...routeProps} />)} />
+            <Route exact path="/Admin" render={(routeProps) => (<Admin lng={lng} {...routeProps} />)} />
+            <Route exact path="/Edit/:editKey" render={(routeProps) => (<EditDeposit lng={lng} {...routeProps} />)}/>
+            <Route exact path="/Forgot" render={(routeProps) => (<ForgetPass lng={lng} {...routeProps} />)}/>
+            <Route exact path="/Admin/Validation" render={(routeProps) => (<ProjectValidation lng={lng} {...routeProps} />)}/>
+            <Route exact path="/login" render={(routeProps) => (<Login lng={lng} {...routeProps} />)}/>
+            <Route exact path="/protected" render={(routeProps) => (<Protected lng={lng} {...routeProps} />)}/>
           </Switch>
         </div>
       </BrowserRouter>
