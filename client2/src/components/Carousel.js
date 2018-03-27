5cef57800324a210ec4f194debff77f5966feba9
Unknown <Cyrinebouz@hotmail.fr>
2018-03-27 22:08:40 +0200

    Carousel updated with links

--- client2/src/components/Carousel.js
+++ client2/src/components/Carousel.js
@@ -2,24 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { AutoRotatingCarousel, Slide } from 'material-auto-rotating-carousel';
 
+
 const items = [
   {
     src: './pictures/project1.jpg',
     altText: 'project1',
     captionHeader: 'BermudZer : pricing d’options bermudéennes',
-    caption: 'Projet d’Innovation Industrielle de 5ème année, Majeure Ingénierie Financière 2014-2015'
+    caption: 'Projet d’Innovation Industrielle de 5ème année, Majeure Ingénierie Financière 2014-2015',
+    link:'https://www.esilv.fr/portfolios/bermudzer-pricing-doption-bermudeennes/'
   },
   {
     src: './pictures/project2.jpg',
     altText: 'project2',
-    captionHeader: 'Application mobile temps réel pour améliorer la qualité de service de transports en commun',
-    caption: 'Projet d’Innovation Industrielle de 5ème année, Majeure Informatique, Big Data et Objets connectés 2014-2015'
+    captionHeader: 'Vinci Eco Drive – Conception et pilotage du Bloc moteur, Shell Eco-Marathon 2015',
+    caption: 'Projet d’Innovation Industrielle de 5ème année, Majeure Mécanique numérique et modélisation 2014-2015',
+    link:'https://www.esilv.fr/portfolios/vinci-eco-drive-conception-et-pilotage-du-bloc-moteur-shell-eco-marathon-2015/'
   },
   {
     src: './pictures/project3.jpg',
     altText: 'Project3',
     captionHeader: 'ID-cam : accessibilité numérique pour les non-voyants et mal-voyants',
-    caption: 'Projet 2017-2018 de 5e année du cursus d’élève ingénieur de l’ESILV, promo 2018'
+    caption: 'Projet 2017-2018 de 5e année du cursus d’élève ingénieur de l’ESILV, promo 2018',
+    link:'https://www.esilv.fr/portfolios/id-cam-accessibilite-numerique-non-voyants-mal-voyants/'
+  },
+  {
+    src: './pictures/project4.jpg',
+    altText: 'Project4',
+    captionHeader: 'Analyse Big Data des sites Unesco via les réseaux sociaux',
+    caption: 'Projet d’Innovation Industrielle de 5ème année, Majeure Informatique et sciences du numérique 2014-2015',
+    link:'https://www.esilv.fr/portfolios/analyse-big-data-des-sites-unesco-via-les-reseaux-sociaux/'
   }
 ];
 
@@ -37,13 +48,13 @@ export default class Caroussel extends React.Component {
 
     const slides = items.map((item) => {
       return (
-        <Link to = "#">
-          <Slide
+        <a href ='' onClick={() => window.location.href=item.link} >
+          <Slide 
             media={<img src={item.src} />}
             title={item.captionHeader}
             subtitle={item.caption}
             key={item.src} />
-        </Link>
+        </a>
       );
     });
 
